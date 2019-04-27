69ca22f36ad173a4c18be8c5dea9788ca7ef1908
Andy <ahua049@aucklanduni.ac.nz>
2019-04-28 04:03:24 +1200

    new driver

--- src/shiftman/client/Driver1.java
+++ src/shiftman/client/Driver1.java
@@ -24,12 +24,12 @@ public class Driver1 {
 	 * manager (12:00-13:00) who has lunch 13:00-14:00 and is worker other times
 	 */
 	private static final String[][] OPENING_HOURS = {
+			{ "Monday", "09:00", "17:00", "", STAFF[0][0], STAFF[0][1], STAFF[1][0], STAFF[1][1] },
+			{ "Tuesday", "09:00", "17:00", "", STAFF[0][0], STAFF[0][1], STAFF[1][0], STAFF[1][1] },
+			{ "Wednesday", "09:00", "17:00", "", STAFF[0][0], STAFF[0][1], STAFF[1][0], STAFF[1][1] },
+			{ "Thursday", "09:00", "21:00", "late", STAFF[0][0], STAFF[0][1], STAFF[1][0], STAFF[1][1] },
 			{ "Friday", "09:00", "17:00", "", STAFF[0][0], STAFF[0][1], STAFF[1][0], STAFF[1][1] },
 			{ "Sunday", "09:00", "17:00", "", STAFF[0][0], STAFF[0][1], STAFF[1][0], STAFF[1][1] },
-			{ "Tuesday", "09:00", "17:00", "", STAFF[0][0], STAFF[0][1], STAFF[1][0], STAFF[1][1] },
-			{ "Thursday", "09:00", "21:00", "late", STAFF[0][0], STAFF[0][1], STAFF[1][0], STAFF[1][1] },
-			{ "Monday", "09:00", "17:00", "", STAFF[0][0], STAFF[0][1], STAFF[1][0], STAFF[1][1] },
-			{ "Wednesday", "09:00", "17:00", "", STAFF[0][0], STAFF[0][1], STAFF[1][0], STAFF[1][1] },
 	};
 	private static final String[][] SHIFT_HOURS = {
 			{ "09:00", "12:00", "" },
@@ -51,7 +51,7 @@ public class Driver1 {
 			scheduler.setWorkingHours(dayspec[0], dayspec[1], dayspec[2]);
 			int normal = (dayspec[3].equals(""))?1:0;
 			for (int i = 0; i < SHIFT_HOURS.length-normal; i++) {
-				scheduler.addShift(dayspec[0], SHIFT_HOURS[i][0], SHIFT_HOURS[i][1], "2");
+				scheduler.addShift(dayspec[0], SHIFT_HOURS[i][0], SHIFT_HOURS[i][1], "1");
 				if (SHIFT_HOURS[i][2].equals("")) {
 					checkStatus("Assign manager", scheduler.assignStaff(dayspec[0], SHIFT_HOURS[i][0], SHIFT_HOURS[i][1], dayspec[4], dayspec[5], true));
 					checkStatus("Assign worker", scheduler.assignStaff(dayspec[0], SHIFT_HOURS[i][0], SHIFT_HOURS[i][1], dayspec[6], dayspec[7], false));					
@@ -68,25 +68,23 @@ public class Driver1 {
 		checkStatus("Assign late night shopping worker", scheduler.assignStaff(OPENING_HOURS[3][0], SHIFT_HOURS[4][0], SHIFT_HOURS[4][1], STAFF[3][0], STAFF[3][1], false));
 		
 		// Provide various reports on current roster
-		report("Registered Employee:", scheduler.getRegisteredStaff());
-		report("Unassigned Employee:", scheduler.getUnassignedStaff());
+		report("Registered Staff:", scheduler.getRegisteredStaff());
+		report("Unassigned Staff:", scheduler.getUnassignedStaff());
 		System.out.println("No managers: " + scheduler.shiftsWithoutManagers());
 		System.out.println("Understaffed: " + scheduler.understaffedShifts());
 		System.out.println("Overstaff: " + scheduler.overstaffedShifts());
-		for (String[] dayspec: OPENING_HOURS) {
-			String day = dayspec[0];
-			report("Roster for " + day, scheduler.getRosterForDay(day));
+		for (String[] staff: STAFF) {
+			String staffName = staff[0] + " " + staff[1];
+			report("Worker roster for " + staffName, scheduler.getRosterForWorker(staffName));
 		}
 		for (String[] staff: STAFF) {
 			String staffName = staff[0] + " " + staff[1];
 			report("Manager roster for " + staffName, scheduler.getShiftsManagedBy(staffName));
 		}
-		for (String[] staff: STAFF) {
-			String staffName = staff[0] + " " + staff[1];
-			report("Worker roster for " + staffName, scheduler.getRosterForWorker(staffName));
+		for (String[] dayspec: OPENING_HOURS) {
+			String day = dayspec[0];
+			report("Roster for " + day, scheduler.getRosterForDay(day));
 		}
-
-
 	}
 	/**
 	 * Helper method for when a String is returned
